96788794c51c9f1ac3de105fe64195f51a96a1ce
Kemal ÇOLAK <55618953+colakcode@users.noreply.github.com>
2020-08-25 11:56:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ $(document).ready(function(){
 $(document).ready(function(){     
   $("#get-footer").load("footer.html");
 });
+
+$(document).ready(function(){     
+  $("#get-topnav").load("topnav.html");
+});
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
