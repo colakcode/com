76a878c170edb45478d8f23559f3c41e73bbc6f5
Kemal Çolak <kemallcolak@gmail.com>
2024-12-08 19:47:51 +0300

    3d models added 081224T1947

--- yada/js/call_contents.js
+++ yada/js/call_contents.js
@@ -5,3 +5,7 @@ $(document).ready(function(){
   $(document).ready(function(){     
     $("#get-free-assets").load("/yada/contents/free_assets.html");
   });
+
+  $(document).ready(function(){     
+    $("#get-3d-models").load("/yada/contents/3d-models.html");
+  });
